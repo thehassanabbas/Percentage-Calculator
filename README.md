6bc87a01ea115904707232715fead1d79698cbc8
Hassan Abbas <mr.hassan14273@gmail.com>
2024-07-09 03:06:01 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Percentage-Calculator
+ It's a simple Grade & Percentage Calculator developed in Plain HTML, CSS, JS.
---
+++ script.js
@@ -0,0 +1,34 @@
+function calculatePercentage() {
+    let FullName = document.getElementById("fullName").value;
+    let TotalMarks = document.getElementById("totalMarks").value;
+    let ObtainedMarks = document.getElementById("obtainedMarks").value;
+
+    percentage = (ObtainedMarks / TotalMarks) * 100;
+    percentage = percentage.toFixed(2);
+
+    let grade = "";
+
+    if (percentage >= 90) {
+        grade = "A+";
+    }
+    else if (percentage >= 80) {
+        grade = "A";
+    }
+    else if (percentage >= 70) {
+        grade = "A-";
+    }
+    else if (percentage >= 60) {
+        grade = "B";
+    }
+    else if (percentage >= 50) {
+        grade = "C";
+    }
+    else if (percentage >= 40) {
+        grade = "D";
+    }
+    else {
+        grade = "F";
+    }
+
+    document.getElementById("result").innerHTML = `${FullName} ! You have successfully achieved ${grade} grade with a commendable score of ${percentage}%`;
+}
